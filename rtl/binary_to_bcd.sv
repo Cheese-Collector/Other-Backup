d394fb1405e49937c45ed453f191e91d836a215a
Cheese-Collector <jacksonjones@student.unimelb.edu.au>
2026-05-20 06:32:39 +0000

    Binary to BCD using double-dabble

--- rtl/binary_to_bcd.sv
+++ rtl/binary_to_bcd.sv
@@ -1,7 +1,28 @@
 `timescale 1ns / 1ps
 
 module binary_to_bcd (
-    input   logic [6:0] bin,    //Binary input, 0-99
-    output  logic [3:0] tens,   //Decimal tens digit (BCD)
-    output  logic [3:0] ones    //Decimal ones digit (BCD)
+    input  logic [6:0] bin,   //Binary input, 0-99
+    output logic [3:0] tens,  //Decimal tens digit (BCD)
+    output logic [3:0] ones   //Decimal ones digit (BCD)
 );
+  logic [6:0] combin;
+  initial begin
+    tens   = 4'b0000;
+    ones   = 4'b0000;
+    combin = bin;
+  end
+
+  always @(bin) begin
+    tens   = 4'b0000;
+    ones   = 4'b0000;
+    combin = bin;
+    repeat (7) begin
+      if (tens >= 5) tens = tens + 4'b0011;
+      if (ones >= 5) ones = ones + 4'b0011;
+      tens   = (tens << 1) + {3'b000, ones[3]};
+      ones   = (ones << 1) + {3'b000, combin[6]};
+      combin = (combin << 1);
+    end
+  end
+
+endmodule
