0091ab943868acb1e38bd42360ac3bf602ffe0ff
Cheese-Collector <jacksonjones@student.unimelb.edu.au>
2026-05-23 12:56:28 +0000

    Finished part1

---
+++ rtl/up_down_counter_rst.sv
@@ -0,0 +1,43 @@
+//Counter than can count up and down for 0 to MAX
+//with enable pin
+//
+//Parameters:
+//MAX   - the max value stored in decimal
+//WIDTH - number of binary digits to store max
+//
+//Ports:
+//clk               - standard clock signal
+//enable            - enable pin. only count when on
+//up                - increment if up is true, decrement if false
+//count [WIDTH-1:0] - count output
+`timescale 1ns / 1ps
+
+
+module up_down_counter_rst #(
+    parameter int MAX   = 2,
+    parameter int WIDTH = 2
+) (
+    input logic clk,
+    input logic rst,
+    input logic enable,
+    input logic up,
+    output logic [WIDTH-1:0] count
+);
+
+  logic [WIDTH-1:0] next_count;
+  always_ff @(posedge clk) begin
+    if (rst) count <= '0;
+    else if (enable) count <= next_count;
+  end
+
+  // span of count set to 0 using "'"
+  initial count = '0;
+
+  //Restricts MAX 32bit width to new variable Max with 'WIDTH'bit width
+  localparam logic [WIDTH-1:0] Max = WIDTH'(MAX);
+
+  always_comb begin
+    if (up) next_count = count < Max ? count + WIDTH'(1) : '0;
+    else next_count = count > 0 ? count - WIDTH'(1) : Max;
+  end
+endmodule
